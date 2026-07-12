f806f29fc4105ddd30db1a60204dda0705c54dbc
CACO Steel <gunuz@cacosteel.com>
2026-07-12 13:04:12 +0300

    Add sitemap, robots.txt and SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: "https://www.cacosteel.com",
+
+  integrations: [
+    sitemap()
+  ]
 });
